5f0c6e8e5749216c5b713646a8168fac8f61a246
Alejandro GX <alex@flyingpigs.es>
2018-08-03 12:02:45 +0200

    minor fixes

--- widgets/hello-world-widget/hello-world-widget.php
+++ widgets/hello-world-widget/hello-world-widget.php
@@ -3,12 +3,12 @@
 /*
 Widget Name: Hello World Widget
 Description: An example widget'.
-Author: Flyingpigs
+Author: Flying Pigs
 Author URI: http://flyingpigs.es
 */
 
 class Hello_World_Widget extends SiteOrigin_Widget {
-	function __construct() {
+  function __construct() {
     //Here you can do any preparation required before calling the parent constructor, such as including additional files or initializing variables.
 
     //Call the parent constructor with the required arguments.
@@ -22,7 +22,7 @@ class Hello_World_Widget extends SiteOrigin_Widget {
       // The $widget_options array, which is passed through to WP_Widget.
       // It has a couple of extras like the optional help URL, which should link to your sites help or support page.
       array(
-          'description' => __('A hello world widget.', 'hello-world-widget')
+        'description' => __('A hello world widget.', 'hello-world-widget')
       ),
 
       //The $control_options array, which is passed through to WP_Widget
@@ -32,72 +32,71 @@ class Hello_World_Widget extends SiteOrigin_Widget {
 
       //The $form_options array, which describes the form fields used to configure SiteOrigin widgets. We'll explain these in more detail later.
       array(
-					'image_url' => array(
-						'type' => 'media',
-						'label' => __('Image file', 'flyingpigs-custom-widgets'),
-						'library' => 'image',
-						'fallback' => true,
-					),
-          'title' => array(
-              'type' => 'text',
-              'label' => __('Title', 'flyingpigs-custom-widgets'),
-              'default' => ''
-          ),
-					'show_text' => array(
-						'type' => 'checkbox',
-						'default' => true,
-						'label' => __('Show text', 'flyingpigs-custom-widgets'),
-					),
-          'text' => array(
-              'type' => 'text',
-              'label' => __('Text', 'flyingpigs-custom-widgets'),
-              'default' => ''
-          ),
+        'image_url' => array(
+          'type' => 'media',
+          'label' => __('Image file', 'flyingpigs-custom-widgets'),
+          'library' => 'image',
+          'fallback' => true,
+        ),
+        'title' => array(
+          'type' => 'text',
+          'label' => __('Title', 'flyingpigs-custom-widgets'),
+          'default' => ''
+        ),
+        'show_text' => array(
+          'type' => 'checkbox',
+          'default' => true,
+          'label' => __('Show text', 'flyingpigs-custom-widgets'),
+        ),
+        'text' => array(
+          'type' => 'text',
+          'label' => __('Text', 'flyingpigs-custom-widgets'),
+          'default' => ''
+        ),
       ),
 
       //The $base_folder path string.
       plugin_dir_path(__FILE__)
     );
-	}
+  }
 
-	function initialize() {
-		$this->register_frontend_scripts(
-			array(
-				array(
-					'hello-world-widget',
-					plugin_dir_url( __FILE__ ) . 'js/hello-world-widget.js',
-					array( 'jquery' ),
-					'1.0'
-				)
-			)
-		);
-	}
+  function initialize() {
+    $this->register_frontend_scripts(
+      array(
+        array(
+          'hello-world-widget',
+          plugin_dir_url( __FILE__ ) . 'js/hello-world-widget.js',
+          array( 'jquery' ),
+          '1.0'
+        )
+      )
+    );
+  }
 
-	function get_template_variables($instance) {
-		$vars = [];
-		$vars['title'] = $instance['title'];
-		$vars['text'] = $instance['text'];
-		$vars['show_text'] = $instance['show_text'];
-		$vars['image_url'] = '';
+  function get_template_variables($instance) {
+    $vars = [];
+    $vars['title'] = $instance['title'];
+    $vars['text'] = $instance['text'];
+    $vars['show_text'] = $instance['show_text'];
+    $vars['image_url'] = '';
 
-		$image = wp_get_attachment_image_src($instance['image_url'], 'full', false);
-		if ($image){
-			$vars['image_url'] = $image[0];
-		}
-		else {
-			$vars['image_url'] = $instance['image_url_fallback'];
-		}
+    $image = wp_get_attachment_image_src($instance['image_url'], 'full', false);
+    if($image) {
+      $vars['image_url'] = $image[0];
+    }
+    else {
+      $vars['image_url'] = $instance['image_url_fallback'];
+    }
 
-		return $vars;
-	}
+    return $vars;
+  }
 
-	function get_template_name($instance) {
-		return 'hello-world-widget-template';
-	}
+  function get_template_name($instance) {
+    return 'hello-world-widget-template';
+  }
 
-	function get_style_name($instance) {
-		return 'hello-world-widget-style';
-	}
+  function get_style_name($instance) {
+    return 'hello-world-widget-style';
+  }
 }
-
 siteorigin_widget_register('hello-world-widget', __FILE__, 'Hello_World_Widget');
