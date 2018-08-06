7e9d9e81b9186f48932cd69f11102ace6037bf20
Alejandro GX <alex@flyingpigs.es>
2018-08-06 14:21:23 +0200

    minor php adjustment (claim)

--- widgets/claim-widget/tpl/claim-widget-template.php
+++ widgets/claim-widget/tpl/claim-widget-template.php
@@ -1,19 +1,19 @@
-<?php if($veil){ ?>
+<?php if($veil): ?>
   <div class="fp_claim" style="background-image: url('<?php echo $image_url; ?>');">
-<?php } else { ?>
+<?php else: ?>
   <div class="fp_claim fp_claim--no-veil" style="background-image: url('<?php echo $image_url; ?>');">
-<?php } ?>
+<?php endif; ?>
   <div class="container">
     <div class="row">
       <div class="col-sm-12">
         <div class="fp_claim__content">
           <h2 class="fp_claim__content__title"><?php echo $claim; ?></h2>
-          <?php if($text != ''){ ?>
+          <?php if($text != ''): ?>
             <p class="fp_claim__content__text"><?php echo $text; ?></p>
-          <?php } ?>
-          <?php if($cta_text != '' && $cta_url != ''){ ?>
-            <a href="<?php echo $cta_url; ?>" class="btn btn--ghost fp_claim__content__cta" <?php if($new_window){ ?> target="_blank" <?php } ?>><?php echo $cta_text; ?></a>
-          <?php } ?>
+          <?php endif; ?>
+          <?php if($cta_text != '' && $cta_url != ''): ?>
+            <a href="<?php echo $cta_url; ?>" class="btn btn--ghost fp_claim__content__cta" <?php if($new_window): ?> target="_blank" <?php endif; ?>><?php echo $cta_text; ?></a>
+          <?php endif; ?>
         </div>
       </div>
     </div>
