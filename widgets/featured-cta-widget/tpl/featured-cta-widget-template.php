a09187ac2b086f364fdfde70d027c1567b835c67
Alejandro GX <alex@flyingpigs.es>
2018-08-07 09:40:05 +0200

    featured cta WIP

--- widgets/featured-cta-widget/tpl/featured-cta-widget-template.php
+++ widgets/featured-cta-widget/tpl/featured-cta-widget-template.php
@@ -6,7 +6,7 @@
   <div class="container">
     <?php if ($icon): ?>
       <div class="row justify-content-center">
-        <div class="col-md-6 col-sm-9">
+        <div class="col-lg-6 col-md-9">
           <div class="fp_featured-cta__icon">
             <?php echo siteorigin_widget_get_icon( $icon ); ?>
           </div>
@@ -14,7 +14,7 @@
       </div>
     <?php endif; ?>
     <div class="row justify-content-center">
-      <div class="col-md-6 col-sm-9">
+      <div class="col-lg-6 col-md-9">
         <h3 class="fp_featured-cta__main"><?php echo $main; ?></h3>
         <?php if($desc != ''): ?>
           <p class="fp_featured-cta__desc"><?php echo $desc; ?></p>
@@ -23,9 +23,11 @@
     </div>
   </div>
   <div class="row justify-content-center">
-    <a href="<?php echo $cta_url; ?>" <?php if($new_window): ?> target="_blank" <?php endif; ?>>
-      <?php echo $cta_text; ?>
-    </a>
+    <div class="col-lg-6 col-md-9">
+      <a href="<?php echo $cta_url; ?>" class="fp_featured-cta__cta btn btn--primary btn--block" <?php if($new_window): ?> target="_blank" <?php endif; ?>>
+        <?php echo $cta_text; ?>
+      </a>
+    </div>
   </div>
 </div>
 
