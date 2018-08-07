5004f2703ec8563e6cd8fe14a1418febc09b7329
Alejandro GX <alex@flyingpigs.es>
2018-08-07 11:49:38 +0200

    regular cta WIP

--- widgets/regular-cta-widget/tpl/regular-cta-widget-template.php
+++ widgets/regular-cta-widget/tpl/regular-cta-widget-template.php
@@ -3,21 +3,19 @@
   <?php if($background != 'white'): ?> fp_regular-cta--colored <?php endif; ?>
   <?php if($bordered): ?> fp_regular-cta--bordered <?php endif; ?>
 ">
-  <div class="container-fluid">
-    <div class="row no-gutter">
-      <div class="col-sm-12">
-        <div class="fp_regular-cta--wrapper">
-          <h3 class="fp_regular-cta__main"><?php echo $main; ?></h3>
-          <?php if($background != 'white'): ?>
-            <a href="<?php echo $cta_url; ?>" class="fp_regular-cta__cta btn btn--block" <?php if($new_window): ?> target="_blank" <?php endif; ?>>
-              <?php echo $cta_text; ?>
-            </a>
-          <?php else: ?>
-            <a href="<?php echo $cta_url; ?>" class="fp_regular-cta__cta btn btn--primary btn--block" <?php if($new_window): ?> target="_blank" <?php endif; ?>>
-              <?php echo $cta_text; ?>
-            </a>
-          <?php endif; ?>
-        </div>
+  <div class="row no-gutters">
+    <div class="col-sm-12">
+      <div class="fp_regular-cta--wrapper">
+        <h3 class="fp_regular-cta__main"><?php echo $main; ?></h3>
+        <?php if($background != 'white'): ?>
+          <a href="<?php echo $cta_url; ?>" class="fp_regular-cta__cta btn btn--block" <?php if($new_window): ?> target="_blank" <?php endif; ?>>
+            <?php echo $cta_text; ?>
+          </a>
+        <?php else: ?>
+          <a href="<?php echo $cta_url; ?>" class="fp_regular-cta__cta btn btn--primary btn--block" <?php if($new_window): ?> target="_blank" <?php endif; ?>>
+            <?php echo $cta_text; ?>
+          </a>
+        <?php endif; ?>
       </div>
     </div>
   </div>
