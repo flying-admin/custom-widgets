f52f3d7b6783bfa1ab9c825275b475d63b9b63e6
Alejandro GX <alex@flyingpigs.es>
2018-08-07 11:49:23 +0200

    featured cta fix

--- widgets/featured-cta-widget/tpl/featured-cta-widget-template.php
+++ widgets/featured-cta-widget/tpl/featured-cta-widget-template.php
@@ -3,26 +3,24 @@
   <?php if($background != 'white'): ?> fp_featured-cta--colored <?php endif; ?>
   <?php if($bordered): ?> fp_featured-cta--bordered <?php endif; ?>
 ">
-  <div class="container">
-    <?php if ($icon): ?>
-      <div class="row justify-content-center">
-        <div class="col-xl-6 col-lg-8 col-md-10">
-          <div class="fp_featured-cta__icon">
-            <?php echo siteorigin_widget_get_icon( $icon ); ?>
-          </div>
+  <?php if ($icon): ?>
+    <div class="row justify-content-center no-gutters">
+      <div class="col-xl-6 col-lg-8 col-md-10">
+        <div class="fp_featured-cta__icon">
+          <?php echo siteorigin_widget_get_icon( $icon ); ?>
         </div>
       </div>
-    <?php endif; ?>
-    <div class="row justify-content-center">
-      <div class="col-xl-6 col-lg-8 col-md-10">
-        <h3 class="fp_featured-cta__main"><?php echo $main; ?></h3>
-        <?php if($desc != ''): ?>
-          <p class="fp_featured-cta__desc"><?php echo $desc; ?></p>
-        <?php endif; ?>
-      </div>
+    </div>
+  <?php endif; ?>
+  <div class="row justify-content-center no-gutters">
+    <div class="col-xl-6 col-lg-8 col-md-10">
+      <h3 class="fp_featured-cta__main"><?php echo $main; ?></h3>
+      <?php if($desc != ''): ?>
+        <p class="fp_featured-cta__desc"><?php echo $desc; ?></p>
+      <?php endif; ?>
     </div>
   </div>
-  <div class="row justify-content-center">
+  <div class="row justify-content-center no-gutters">
     <div class="col-xl-6 col-lg-8 col-md-10">
       <?php if($background != 'white'): ?>
         <a href="<?php echo $cta_url; ?>" class="fp_featured-cta__cta btn btn--block" <?php if($new_window): ?> target="_blank" <?php endif; ?>>
