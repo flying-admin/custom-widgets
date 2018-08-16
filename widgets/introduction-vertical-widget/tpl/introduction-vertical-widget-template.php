cb0bc75857caefbe999c4d127163ee06952ec661
Cristina <>
2018-08-16 14:14:37 +0200

    WIP video module

--- widgets/introduction-vertical-widget/tpl/introduction-vertical-widget-template.php
+++ widgets/introduction-vertical-widget/tpl/introduction-vertical-widget-template.php
@@ -2,7 +2,7 @@
   <div class="container">
       <div class="fp_introduction-v__content">
         <div class="row">
-          <div class="col-lg-8 col-sm-12">
+          <div class="col-sm-8">
             <h2 class="fp_introduction-v__content__title">
               <?php echo $title; ?>
             </h2>
@@ -17,21 +17,22 @@
             </div>
             <?php endif; ?>
           </div>
-          <div class="col-sm-12 col-lg-4">
+          <div class="col-sm-4 text-sm-right">
             <?php if( $image_url != '' || $link ): ?>
-              <div class="col-sm-12 ">
                   <?php if($image_url != ''): ?>
-                      <figure class="fp_introduction-v__content__img content_image" data-width="740" data-height="480" >
-                        <img src="<?php echo $image_url; ?>" class="">
-                      </figure>
+                  <figure class="container__image container__image--v fp_introduction-v__content__img">
+                    <div class="content_image" data-width="1300" data-height="481">
+                      <img src="<?php echo $image_url; ?>" class="">          
+                    </div>
+                </figure>
                   <?php endif; ?>
+                  
                   <?php if( $link ): ?>
                     <a href="<?php echo $link_url; ?>" class="link link--primary link--external fp_introduction-v__content__cta" <?php if($link_blank): ?> target="_blank" <?php endif; ?>>
                       <?php echo $link_text; ?>
                       <?php echo siteorigin_widget_get_icon( 'genericons-external' ); ?>
                     </a>
                   <?php endif; ?>
-              </div>
             <?php endif; ?>
           </div>
         </div>
