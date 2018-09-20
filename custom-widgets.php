517c22fd63d72a0f5155484036c2af6eddbd682c
fp-ultano <ultano@flyingpigs.es>
2018-09-20 13:15:51 +0200

    fixed errors

--- custom-widgets.php
+++ custom-widgets.php
@@ -79,12 +79,12 @@ function init_cta_form_send_form (){
     if ($_REQUEST["action"] == "ie_exec_cta_formulario"){
 
       // Get the form fields and remove whitespace.
-       $name = strip_tags(trim($_POST["emaildata"]['username']));
+       $name = strip_tags(trim($_POST["emaildata"]['userfirstname']));
        $name = str_replace(array("\r","\n"),array(" "," "),$name);
        $userlastname = strip_tags(trim($_POST["emaildata"]['userlastname']));
        $userlastname = str_replace(array("\r","\n"),array(" "," "),$userlastname);
        $name .= " ".$userlastname;
-       $email = filter_var(trim( $_POST["emaildata"]['usermail'] ), FILTER_SANITIZE_EMAIL);
+       $email = filter_var(trim( $_POST["emaildata"]['useremail'] ), FILTER_SANITIZE_EMAIL);
        $message = trim( $_POST["emaildata"]['usermessage']);
 
 
@@ -92,7 +92,7 @@ function init_cta_form_send_form (){
       if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
         // Set a 400 (bad request) response code and exit.
         http_response_code(400);
-        echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+        echo __('Oops! Something went wrong, please try again later', 'custom-widgets');
         exit;
       }
 
@@ -106,8 +106,11 @@ function init_cta_form_send_form (){
       $email_content .= "Email: $email\n\n";
       $email_content .= "Message:\n$message\n\n";
       $email_content .= "Mesasge info: \n eventurl: ".$_POST["emaildata"]['eventurl']."\n eventtopic: ".$_POST["emaildata"]['eventtopic'];
-
-      wp_mail( $recipient, $subject ,$email_content , "",array() );
+      $resultado = wp_mail( $recipient, $subject ,$email_content);
+      if(!$resultado){
+        http_response_code(400);
+        echo __('Oops! Something went wrong, please try again later', 'custom-widgets');
+      }
 
       exit();
     }
